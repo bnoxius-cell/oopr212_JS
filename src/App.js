0f09c4eb6ca694fa3bed45ec7f36a4de5dc4a7cc
bnoxius-cell <idevadrian@gmail.com>
2026-01-30 08:38:01 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      </div>
+      <Banner2/>
+      </div>  
   );
 }
 
---
+++ src/components/banner2/data/data.js
@@ -0,0 +1,11 @@
+import img1 from './images/img1.png';
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
+
+const bannerImages = [ 
+    img1,
+    img2,
+    img3,
+]
+
+export default bannerImages;
